b2d689e2db10d143a5e3b80d40a38c7107b7b3fb
Christoph Sens-Schoenfelder <sens-schoenfelder@gfz-potsdam.de>
2015-12-10 11:22:18 +0100

    ENH: Add funtionality to read data from filesystem with given directory structure and file name format

--- miic.core/src/miic/core/stream.py
+++ miic.core/src/miic/core/stream.py
@@ -15,6 +15,11 @@ Created on Apr 2, 2012
 import numpy as np
 from multiprocessing import Pool
 from copy import deepcopy
+import datetime
+import glob
+import re
+import os
+
 
 # ETS imports
 try:
@@ -27,6 +32,7 @@ except ImportError:
     pass
     
 # Obspy import
+from obspy import read
 from obspy.core.stream import Stream
 from obspy.core.trace import Trace
 from obspy.core import Stats
@@ -677,5 +683,203 @@ def trace_sym_pad_shrink_to_npts(tr, npts):
     tr.stats['npts'] = npts
     tr_padded = tr
     return tr_padded
+
+
+IDformat = ['%NET','%STA','%LOC','%CHA']
+
+def read_from_filesystem(ID,starttime,endtime,fs):
+    """Function to read data from a filesystem with a give file structure
+    just by specifying ID and time interval.
+
+    :param ID: seedID of the channel to read (NET.STA.LOC.CHA)
+    :type ID: string
+    :param starttime: start time
+    :type starttime: datetime.datetime
+    :param endtime: end time
+    :type endtime: datetime.datetime
+    :param fs: file structure descriptor
+    :type fs: list
+
+    **File structure descriptor**
+    fs is a list of strings or other lists indicating the elements in the
+    file structure. Each item of this list is translated in one level of the
+    directory structure. The first element is a string indicating the
+    base_directory. The following elements can be strings to indicate
+    one of the following
+     - %X as defined by datetime.strftime indicating an element of
+       the time. e.g. %H
+     - %NET: network name
+     - %STA: station name
+     - %CHA: channel name
+     - %LOC: location
+     - string with out %
+    The format strings are replaced either with an element of the starttime
+    if they correspond to a datetime specifyer or with the respective part
+    of the seedID. A string withouta % sign is not changed. If more than one
+    format string is required in one directory level the need to be
+    separated within a sublist.
+
+    A format string for the ID can be followed by a braces including two
+    strings that will be used to replace the first string with the second.
+    This cna be used if the filename contains part of the ID in a different
+    form.
+
+    :Exapmple:
+
+    Example for a station 'GJE' in network 'HEJSL' with channel 'BHZ' and
+    location '00' with the start time 2010-12-24_11:36:30 and
+    fs = ['base_dir','%Y','%b','%NET,['%j','_','%STA'','_T_','%CHA('BH','')',
+                                  '.mseed']]
+    will be translated in a linux filename
+    'base_dir/2010/Nov/HEJSL/317_GJE_T_Z.mseed'
+    """
+
+    # translate file structure string
+    fpattern = _current_filepattern(ID,starttime,fs)
+    st = _read_filepattern(fpattern, starttime, endtime)
+
+    # if trace starts too late have a look in the previous section
+    if (st[0].stats.starttime-st[0].stats.delta).datetime > starttime:
+        fpattern = _adjacent_filepattern(ID,starttime,fs,-1)
+        st += _read_filepattern(fpattern, starttime, endtime)
+        st.merge()
+    thistime = starttime
+    while (st[0].stats.endtime.datetime < endtime) & (thistime < endtime):
+        fpattern, thistime = _adjacent_filepattern(ID,thistime,fs,1)
+        if thistime == starttime:
+            break
+        st += _read_filepattern(fpattern, starttime, endtime)
+        st.merge()
+    st.trim(starttime=UTCDateTime(starttime),endtime=UTCDateTime(endtime))
+    st = st.select(id=ID)
+    return st
+
+
+def _read_filepattern(fpattern, starttime, endtime):
+    """Read a stream from files whose names match a given pattern.
+    """
+    flist = glob.glob(fpattern)
+    starttimes = []
+    endtimes = []
+    # first only read the header information
+    for fname in flist:
+        st = read(fname,headonly=True).merge()
+        starttimes.append(st[0].stats.starttime.datetime)
+        endtimes.append(st[0].stats.endtime.datetime)
+    # now read the stream from the files that contain the perior
+    st = Stream()
+    for ind,fname in enumerate(flist):
+        if (starttimes[ind] < endtime) and (endtimes[ind] > starttime):
+            st += read(fname,starttime=starttime,endtime=endtime)
+    st.merge()
+    return st
+
+
+def _adjacent_filepattern(ID,starttime,fs,inc):
+    """Return the file name that contains the data sequence prior to the one
+    that contains the given time for a given file structure and ID.
+
+    :param inc: either 1 for following of -1 for previous period
+    :type inc: int
+    """
+    assert ((inc == 1) or (inc == -1)), " inc must either be 1 or -1"
+    fname = ''
+    flag = 0
+    # find earlier time by turning back the time by one increment of the
+    # last time indicator in the fs
+    for part in fs[-1::-1]:
+        if not isinstance(part,list):
+            part = [part]
+        for tpart in part[-1::-1]:
+            if (tpart not in IDformat) and ('%' in tpart) and (flag == 0):
+                flag = 1
+                if tpart in ['%H','%I']:
+                    thistime = starttime + inc * datetime.timedelta(hours=1)
+                elif tpart == '%p':
+                    thistime = starttime + inc * datetime.timedelta(hours=12)
+                elif tpart in ['%a','%A','%w','%d','j']:
+                    thistime = starttime + inc * datetime.timedelta(days=1)
+                elif tpart in ['%U','%W']:
+                    thistime = starttime + inc * datetime.timedelta(days=7)
+                elif tpart in ['%b','%B','%m']:
+                    if starttime.month + inc == 0:
+                        thistime = datetime.datetime(starttime.year-1,
+                                                     12,
+                                                     starttime.day,
+                                                     starttime.hour,
+                                                     starttime.minute,
+                                                     starttime.second,
+                                                     starttime.microsecond)
+                    elif starttime.month + inc == 13:
+                        thistime = datetime.datetime(starttime.year+1,
+                                                     1,
+                                                     starttime.day,
+                                                     starttime.hour,
+                                                     starttime.minute,
+                                                     starttime.second,
+                                                     starttime.microsecond)
+                    else:
+                        thistime = datetime.datetime(starttime.year,
+                                                     starttime.month + inc,
+                                                     starttime.day,
+                                                     starttime.hour,
+                                                     starttime.minute,
+                                                     starttime.second,
+                                                     starttime.microsecond)
+                elif tpart in ['%y','%Y']:
+                    thistime = datetime.datetime(starttime.year - inc,
+                                                     starttime.month,
+                                                     starttime.day,
+                                                     starttime.hour,
+                                                     starttime.minute,
+                                                     starttime.second,
+                                                     starttime.microsecond)
+    fname = _current_filepattern(ID,thistime,fs)
+    return fname
+
+
+def _current_filepattern(ID,starttime,fs):
+    """Return the file name that contains the data sequence that contains
+    the given time for a given file structure and ID.
+    """
+    fname = ''
+    for part in fs:
+        if not isinstance(part,list):
+            part = [part]
+        fpartname = ''
+        for tpart in part:
+            fpartname += _fs_translate(tpart,ID,starttime)
+        fname = os.path.join(fname,fpartname)
+    return fname
+
+
+def _fs_translate(part,ID,starttime):
+    """Translate part of the file structure descriptor.
+    """
+    IDlist = ID.split('.')
+    if ('(' in part) and (')' in part):
+        trans = re.search('\(.*?\)',part).group(0)
+    else:
+        trans = None
+    # in case there is something to translate remove from the filepart
+    if trans:
+        part = part.replace(trans,'')
+    # if there is no %-sign it is a fixed string
+    if not '%' in part:
+        res = part
+    # in case it belongs to the ID replace it with the respective ID-part
+    if part in IDformat:
+        res = IDlist[IDformat.index(part)]
+    # otherwise it must be part of the date string
+    else:
+        res = starttime.strftime(part)
+    # replace if nesseccary
+    if trans:
+        transl = trans[1:-1].split(',')
+        assert len(transl) == 2, "%s is not valid for replacement" % trans
+        res = res.replace(transl[0],transl[1])
+    return res
+
+
 
 #EOF
