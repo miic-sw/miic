9abb280efa7d2576ee6b4209d2df8fe8eb18a4de
Robert Green <rgreen@glic2.gfz-hpcc.cluster>
2017-11-13 17:13:05 +0100

    fix create_path modified for parallel calls

--- miic.core/src/miic/core/miic_utils.py
+++ miic.core/src/miic/core/miic_utils.py
@@ -288,11 +288,19 @@ def create_path(directory):
     :return: 0
     """
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists(directory) :
+        # Use try in case another parallel process has made it in that time
+        try :
+            os.makedirs(directory)
+        except OSError as e :
+            if e[0]==17 :
+                pass
+            else :
+                raise OSError(e)
     else:
         assert os.path.isdir(directory), "%s exists but is not a directory" % subpath
     return 0
+
 
 
 def archive_code(scriptname):
