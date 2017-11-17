9c83def4d2cc9daa4af4c4e2eac11fd6f24bf630
robertggreen <rgreen@gfz-potsdam.de>
2017-11-17 16:26:06 +0100

    ADD Remove stations without 3 chnls for joint norm

--- miic.core/src/miic/core/script_utils.py
+++ miic.core/src/miic/core/script_utils.py
@@ -231,26 +231,46 @@ def combine_station_channels(stations,channels,par_co,lle_df):
     return [first, second]
 
 
-def select_available_combinations(st,comb_list):
-   """Estimate available subset of combinations
-   """
-   stations = []
-   combis = []
-   for tr in st:
-       stations.append('%s.%s..%s' %(tr.stats.network,tr.stats.station,tr.stats.channel))
+def select_available_combinations(st,comb_list,targs):
+    """Estimate available subset of combinations
+    """
 
-   for ind in range(len(st)):
-       # find all occurrences of a trace in combinations
-       findex = [ii for ii,x in enumerate(comb_list[0]) if x == stations[ind]]
-       # for every occurrence ...
-       for find in findex:
-           try:
-               # ... check whether the trace that is to be combined is present in the stream
-               sind = stations.index(comb_list[1][find])
-               combis.append((ind,sind))
-           except:
-               pass
+    # For joint normalisation require all three channels to be present
+    # Check 3 channels for each station and if not remove from stream
+    joint_norm=False
+    for proc in targs['FDpreProcessing']:
+        if 'joint_norm' in proc['args'].keys():
+            if proc['args']['joint_norm']==True :
+                joint_norm=True
+    if joint_norm :
+        channels_per_station={}
+        for tr in st:
+            channels_per_station[tr.stats.network+'.'+tr.stats.station]=[]
+        for tr in st:
+            channels_per_station[tr.stats.network+'.'+tr.stats.station].append(tr.stats.channel)
+        for s in channels_per_station.keys() :
+            if not (len(channels_per_station[s]) % 3 == 0) :
+                rem_st=st.select(station=s.split('.')[1])
+                for rem_tr in rem_st.traces :
+                    st.remove(rem_tr)
 
-   return combis
+    stations = []
+    combis = []
+    for tr in st:
+        stations.append('%s.%s..%s' %(tr.stats.network,tr.stats.station,tr.stats.channel))
+
+    for ind in range(len(st)):
+        # find all occurrences of a trace in combinations
+        findex = [ii for ii,x in enumerate(comb_list[0]) if x == stations[ind]]
+        # for every occurrence ...
+        for find in findex:
+            try:
+                # ... check whether the trace that is to be combined is present in the stream
+                sind = stations.index(comb_list[1][find])
+                combis.append((ind,sind))
+            except:
+                pass
+
+    return combis
 
 
